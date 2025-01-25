af0267292c3651a9864f1839062935885460aafe
Ryan Schildknecht <schildknecht618@gmail.com>
2025-01-25 11:47:57 -0600

    Initial Commit

---
+++ Noteify/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Noteify
+//
+//  Created by Ryan Schildknecht on 1/25/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
